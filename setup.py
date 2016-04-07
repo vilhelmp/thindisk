2a851f6ad0bdf7141caccbe09807db3c4fb60024
Magnus Persson <vilhelmp@gmail.com>
2016-04-07 13:05:43 +0200

    added setup.py file for easy development and install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='ThinDisk',
     version='0.0.0',
-    summary='Sebastien Marets Python thin disk model '
+    description='Sebastien Marets Python thin disk model '
             'from https://github.com/smaret/thindisk',
     entry_points = {
         'console_scripts': [
